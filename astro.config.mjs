7b76ea43816f71e9f38b711e43036ebf36b67b1b
Matteo Tagliatti <matteotagliatti@gmail.com>
2024-02-09 17:41:26 +0100

    nav, login and logout, icons, pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), alpinejs()],
+  integrations: [tailwind(), alpinejs(), icon()]
 });
