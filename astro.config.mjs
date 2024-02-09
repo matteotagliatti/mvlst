576fca140357d365166bbd7b1d370a8d3d423f11
Matteo Tagliatti <matteotagliatti@gmail.com>
2024-02-09 17:58:07 +0100

    better htmx setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
 import alpinejs from "@astrojs/alpinejs";
+import tailwind from "@astrojs/tailwind";
+import htmx from "astro-htmx";
+import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), alpinejs(), icon()]
+  integrations: [tailwind(), alpinejs(), icon(), htmx()],
 });
