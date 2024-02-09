498705a886cbe3232d8e59eba77cbe8db481777f
Matteo Tagliatti <matteotagliatti@gmail.com>
2024-02-09 15:58:06 +0100

    better auth via middleware

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,8 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare namespace App {
+  interface Locals {
+    pb: any; // doens't work with a standard import of a custom type. This is the right solution
+  }
+}
--- src/utils/pocketbase.ts
+++
@@ -1,2 +0,0 @@
-import PocketBase from "pocketbase";
-export const pocketbase = new PocketBase(import.meta.env.POCKETBASE_URL);
