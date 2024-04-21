f8e5ee17b2762d402db673982fb6518e0521ca6c
Matteo Tagliatti <matteotagliatti@gmail.com>
2024-04-22 00:26:59 +0200

    Update UI styles in card, layout, input, button components and tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#161616",
+        foreground: "#282828",
+        border: "#343434",
+        headers: "#ededed",
+        text: "#a0a0a0",
+      },
+    },
+  },
+  plugins: [],
+};
